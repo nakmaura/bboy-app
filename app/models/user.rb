d6e3562e79c896c4d2e68a7dacc40b7265c21461
nakmaura <nakamura>
2020-05-14 11:59:49 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  def email_required?
-    false
-  end
-
-  def email_changed?
-    false
-  end
-
-  def will_save_change_to_email?
-    false
-  end
+         
 end
