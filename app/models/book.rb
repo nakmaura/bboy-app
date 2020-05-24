680209e78703098933500b223ac61c97c074ba89
nakmaura <nakamura>
2020-05-24 15:49:39 +0900

    選択式投稿修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ApplicationRecord
   mount_uploader :video, VideoUploader
   validates :video,:old,presence: true
   belongs_to :user
+  enum style: { "": 1, "": 2}, _prefix: true
+  enum old: { "": 1, "": 2}, _prefix: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'books#index'
   resources :users, only: [:show]
-  resources :books
+  resources :books,expect: [:show]
 end
